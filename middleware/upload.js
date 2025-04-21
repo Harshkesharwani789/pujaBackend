a1b2d5b869072dbaae8e19a0c8d8a05d21c4baff
Harshkesharwani789 <atul123564@gmail.com>
2025-04-21 13:50:16 +0530

    5

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,12 +1,21 @@
-// middleware/upload.js
 import multer from "multer";
 import path from "path";
 import fs from "fs";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const dir = "uploads/categories";
-    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+    let dir = "uploads/others"; // default folder
+
+    if (req.originalUrl.includes("/products")) {
+      dir = "uploads/products";
+    } else if (req.originalUrl.includes("/categories")) {
+      dir = "uploads/categories";
+    }
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+
     cb(null, dir);
   },
   filename: function (req, file, cb) {
